a22aa2b9aba0742107e4ce0e25af1a8208162d99
pienthusiast <mborameral@gmail.com>
2017-04-22 22:26:23 +0300

    Update oku_conv.h

--- oku_conv.h
+++ oku_conv.h
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+int fsize(FILE *);
 int meh(void)
 {
-
-    char buffer[32] = {0};  /* initialized to zeroes */
-    char buffer1[290];
-    char buffer2[290];
+	
+    char buffer[231535] = {0};  /* initialized to zeroes */
+    char buffer1[231535];
+  
     int i,j,k,m;
-    FILE *fp = fopen("OWC.txt", "rb");
-
+    FILE *fp = fopen("unixdict.txt", "rb");
+    int size = fsize(fp);
+	FILE *fs = fopen("Binaryswosh.bin","wb");
     if (fp == NULL) {
-        perror("Failed to open file \"myfile\"");
+        perror("Failed to open file ");
         return EXIT_FAILURE;
     }
 
-
-    for (i = 0; i <= 23; i++) {
+    for (i = 0; i <= 231534; i++) {
         char rc = getc(fp);
         if (rc == EOF) {
             fputs("An error occurred while reading the file.\n", stderr);
@@ -26,19 +26,26 @@ int meh(void)
     }
 
     fclose(fp);
-    for(k = 0; k <= (23); k++)
+    
+    for(k = 0; k <= 231534; k++)
     {
 	int a = buffer[k];
 	itoa(a,buffer1,2);
-	printf("Binary %s\n",buffer1);
+	//printf("Binary value is...%s\n",buffer1);
 	}
-	for (m = 0; m <= 290; m++){
-		buffer2[m] = buffer1[m];
+	fwrite(buffer1, sizeof(char),sizeof(buffer1), fs);
+	
+    /*for(j = 0; j <= (231534) ; j++){
+    printf("Value that has been read...%x\n", buffer[j]);
 	}
-	printf("%s",buffer2);
-    for(j = 0; j <= (23) ; j++){
-    printf("The byte read were... %d\n", buffer[j]);
-	}
+    fclose(fs);
+    printf("%d",size);*/
     return EXIT_SUCCESS;
-    
+}
+int fsize(FILE *fp){
+    int prev=ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    int sz=ftell(fp);
+    fseek(fp,prev,SEEK_SET); //go back to where we were
+    return sz;
 }
